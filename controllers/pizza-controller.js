79914a0df5d6d14f6f483a6c3597fd650240599d
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-06-20 18:57:47 -0500

    added the CRUD methods to the pizzacontroller object.

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,7 +1,76 @@
-const { pizza } = require('../models');
+const { Pizza } = require('../models');
 
 const pizzaController = {
     // the functions will go in here as methods
+
+    // get all pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => {
+            console.log(err, 'error');
+            res.status(400).json(err);
+        });
+    },
+
+    // get one Pizza by id
+    getPizzaById({params}, res) {
+        Pizza.findOne({_id: params.id})
+        .then(dbPizzaData => {
+            // if no pizza is found send 404
+            if (!dbPizzaData) {
+                res.status(404).json({message: 'No pizza found with this id'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => {
+            console.log(err, 'error2');
+            res.status(400).json(err);
+        });
+    },
+
+    // create a pizza
+    createPizza({body}, res) {
+        Pizza.create(body)
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => {
+            console.log(err, 'error3');
+            res.status(400).json(err);
+        });
+    },
+
+    // update pizza by id
+    updatePizza({params, body}, res) {
+        Pizza.findOneAndUpdate({_id: params.id}, body, {new: true})
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({message: 'No pizza found with this id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => {
+            console.log(err, 'error4');
+            res.status(400).json(err);
+        });
+    },
+
+    // delete a pizza
+    deltePizza({params}, res) {
+        Pizza.findOneAndDelete({_id: params.id})
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({message: 'No pizza found with this id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => {
+            console.log(err, 'error5');
+            res.status(400).json(err);
+        });
+    }
 };
 
 
